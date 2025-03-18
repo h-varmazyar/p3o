c6a30954028dc74c72f80fe1ce840a0e01d31d2f
hossein varmazyar <hossein.varmazyar@yahoo.com>
2025-03-18 16:04:17 +0000

    refactor

--- internal/controllers/link/create.go
+++ internal/controllers/link/create.go
@@ -2,45 +2,12 @@ package link
 
 import (
 	"github.com/gin-gonic/gin"
-<<<<<<< HEAD
-=======
 	"github.com/h-varmazyar/p3o/internal/domain"
 	"github.com/h-varmazyar/p3o/internal/errors"
->>>>>>> 292128d (feat: add link creation)
 	"github.com/h-varmazyar/p3o/pkg/utils"
 )
 
 func (c *Controller) Create(ctx *gin.Context) {
-<<<<<<< HEAD
-	createLinkReq := new(CreateLinkReq)
-
-	if err := ctx.ShouldBindJSON(&createLinkReq); err != nil {
-		utils.JsonHttpResponse(ctx, nil, ErrInvalidData.AddOriginalError(err), false)
-		return
-	}
-
-	if !isValidLink(createLinkReq.RealUrl) {
-		utils.JsonHttpResponse(ctx, nil, ErrInvalidLink, false)
-		return
-	}
-
-	if createLinkReq.Key == "" {
-		var err error
-		createLinkReq.Key, err = pickKey()
-		if err != nil {
-			utils.JsonHttpResponse(ctx, nil, ErrKeyGenerationFailed, false)
-			return
-		}
-	}
-
-	linkData := &entities.Link{
-		Key:       createLinkReq.Key,
-		RealLink:  createLinkReq.RealUrl,
-		Immediate: true,
-	}
-
-	if err := c.linkModel.Create(ctx, linkData); err != nil {
-=======
 	linkCreateReq := new(domain.LinkCreateReq)
 
 	if err := ctx.ShouldBindJSON(&linkCreateReq); err != nil {
@@ -52,18 +19,9 @@ func (c *Controller) Create(ctx *gin.Context) {
 
 	resp, err:=c.linkService.Create(ctx, *linkCreateReq)
 	if err!=nil{
->>>>>>> 292128d (feat: add link creation)
 		utils.JsonHttpResponse(ctx, nil, err, false)
 		return
 	}
 
-<<<<<<< HEAD
-	resp := &CreateLinkResp{
-		Url:       linkData.RealLink,
-		Key:       linkData.Key,
-		Immediate: linkData.Immediate,
-	}
-=======
->>>>>>> 292128d (feat: add link creation)
 	utils.JsonHttpResponse(ctx, resp, nil, true)
 }
