fa86cd2c057b342a0ae00c15bbff8cdff9eae892
h-varmazyar <hossein.varmazyar@yahoo.com>
2025-04-07 19:41:18 +0330

    fix: change indirect redirect status code

--- internal/controllers/link/indirect_visit.go
+++ internal/controllers/link/indirect_visit.go
@@ -1,7 +1,6 @@
 package link
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/h-varmazyar/p3o/pkg/utils"
 	"net/http"
@@ -11,12 +10,11 @@ func (c Controller) IndirectVisit(ctx *gin.Context) {
 	key := ctx.Param("key")
 	id := ctx.Param("id")
 
-	fmt.Println("sssssssssssssssssssssss")
 	link, err := c.linkService.IndirectVisit(ctx, key, id)
 	if err != nil {
 		utils.JsonHttpResponse(ctx, nil, err, false)
 		return
 	}
 
-	ctx.Redirect(http.StatusMovedPermanently, link.Url)
+	ctx.Redirect(http.StatusPermanentRedirect, link.Url)
 }
