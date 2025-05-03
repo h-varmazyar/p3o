e1019c1138cdfb9a2a4a55a7efae7d18b6a3a183
h-varmazyar <hossein.varmazyar@yahoo.com>
2025-05-03 22:54:26 +0330

    fix(gather): change not found error code

--- internal/errors/repositories.go
+++ internal/errors/repositories.go
@@ -13,7 +13,7 @@ var (
 
 	//postgres errors start from 20050 to 20099
 	ErrFailedToCreateLink    = errors.NewWithCode("repositories.link.create_link_failed", 20050)
-	ErrLinkNotFound          = errors.NewWithCode("repositories.link.link_not_found", 20051)
+	ErrLinkNotFound          = errors.NewWithHttp("repositories.link.link_not_found", 20051, http.StatusNotFound)
 	ErrIncreaseVisitFailed   = errors.NewWithCode("repositories.link.increase_visit_failed", 20052)
 	ErrLinkCountFetchFailed  = errors.NewWithCode("repositories.link.link_count_fetch_failed", 20053)
 	ErrVisitCountFetchFailed = errors.NewWithCode("repositories.link.visit_count_fetch_failed", 20054)
